669f8ba6c80d5632e5baf8854e89a738182fb03a
dekagalang <30586485+dekagalang@users.noreply.github.com>
2017-10-31 09:27:42 +0700

    portfolio

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-$(window).on('load', function() {
+;$(window).on('load', function() {
 	var $menu = $('.list button');
 		$('#div2, #div3').css('display','none');
 		$('#load').fadeOut('slow');
-		$('body').css('overflow','visible')
+		$('body').css('overflow','visible');
 			$('.show').click(function(e){
 				e.preventDefault();
 				$menu.removeClass('active');
